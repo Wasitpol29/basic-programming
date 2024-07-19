b5a2120201c7741f3af88425c1199cb888d5a965
Wasitpol29 <rrq.bf12@gmail.com>
2024-07-19 16:15:49 +0700

    เปรียบเทียบค่า

--- ep1/index.ts
+++ ep1/index.ts
@@ -7,3 +7,10 @@ let อยากกลับบ้าน:boolean = false
 console.log(firstName)
 console.log(y)
 console.log(อยากกลับบ้าน)
+
+let a :number = 29
+let b :number = 12
+
+console.log( a === b)
+console.log( a > b )
+console.log(a <= b )
