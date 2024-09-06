334831d7a937c89cfa07d3d20840c6acf7ca2cf3
Wasitpol <rrq.bf12@gmail.com>
2024-09-06 13:14:10 +0700

    .

---
+++ ep8/index.ts
@@ -0,0 +1,16 @@
+const aboutMe = {
+    firstName: 'Ten',
+    lastName: 'Hag',
+    age: 18 ,
+    telephone: '112',
+    birthday: 29/12/2548,
+    address: 'manchester',
+    gender: 'male',
+    zipcode: '70077007',
+    road: 'antony',
+    email: 'onana100goal@gmail.com',
+    district: 'iloan',
+    country: 'chickenkook'
+}
+
+console.log(aboutMe.firstName)
