a412436ab039b81970ef92a5d26580c4951f402a
Wasitpol <rrq.bf12@gmail.com>
2024-09-06 12:06:25 +0700

    homework

---
+++ ep7/homework.ts
@@ -0,0 +1,15 @@
+const me = {
+    firstName: 'Ten',
+    lastName: 'Hag',
+    age: 18 ,
+    telephone: '112',
+    birthday: 29/12/2548,
+    address: 'manchester',
+    gender: 'male',
+    zipcode: '70077007',
+    road: 'antony',
+    email: 'onana100goal@gmail.com',
+    district: 'iloan',
+    country: 'chickenkook'
+    
+}
